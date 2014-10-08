b5a0531cc78f615269b7b6c731b715db9a5d09cf
jlmegin <jlmegin@wpi.edu>
2014-10-08 14:48:01 -0400

    started epsilon

---
+++ src/hanto/studentmwcjlm/epsilon/EpsilonHantoGame.java
@@ -0,0 +1,61 @@
+package hanto.studentmwcjlm.epsilon;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+import hanto.common.MoveResult;
+import hanto.studentmwcjlm.common.AbstractHantoGame;
+import hanto.studentmwcjlm.common.ComparableHantoCoordinate;
+
+public class EpsilonHantoGame extends AbstractHantoGame {
+
+	/** Create a new epsilon hanto game
+	 * @param firstPlayerColor the color of the player that goes first
+	 */
+	protected EpsilonHantoGame(HantoPlayerColor firstPlayerColor) {
+		super(firstPlayerColor);
+		init();
+	}
+	
+	/** Initialize Epsilon */
+	private void init() {
+		pieceFactory = new EpsilonHantoPieceFactory();
+	}
+
+	/** Defines the starting inventory for this game
+	 * 	
+	 * @return The starting inventory for this game
+	 */	
+	@Override
+	protected Map<HantoPieceType, Integer> getStartingInventory() {
+		HashMap<HantoPieceType, Integer> startingPieces = new HashMap<HantoPieceType, Integer>();
+		startingPieces.put(HantoPieceType.BUTTERFLY, 1);
+		startingPieces.put(HantoPieceType.SPARROW, 2);
+		startingPieces.put(HantoPieceType.CRAB, 6);
+		startingPieces.put(HantoPieceType.HORSE, 4);
+		return startingPieces;
+	}
+	
+	@Override
+	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from,
+			HantoCoordinate to) throws HantoException {
+		//check if the player resigns
+		if(pieceType == null && from == null && to == null) {
+			//TODO: check to see if they have legal moves, if they don't throw new HantoPrematureResignationException
+			resigned = true;
+			return getMoveResult();
+		}
+		ComparableHantoCoordinate toCoord = convertHantoCoordinate(to);
+		ComparableHantoCoordinate fromCoord = null;
+		if (from != null) {
+			 fromCoord = convertHantoCoordinate(from);
+		}
+		return makeMove(pieceFactory.makePiece(currentPlayer.getColor(), pieceType), fromCoord, toCoord);
+		
+	}
+
+}
---
+++ src/hanto/studentmwcjlm/epsilon/EpsilonHantoPieceFactory.java
@@ -0,0 +1,18 @@
+package hanto.studentmwcjlm.epsilon;
+
+import hanto.common.HantoException;
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+import hanto.studentmwcjlm.common.BasicHantoPiece;
+import hanto.studentmwcjlm.common.HantoPieceFactory;
+
+public class EpsilonHantoPieceFactory implements HantoPieceFactory {
+
+	@Override
+	public BasicHantoPiece makePiece(HantoPlayerColor color, HantoPieceType type)
+			throws HantoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
