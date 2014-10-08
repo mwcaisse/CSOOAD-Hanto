cc7ac0e3aea2050f7fd1b8d257dc2ff84c2e5933
Mitchell Caisse <computers1055@gmail.com>
2014-10-08 18:31:54 -0400

    Updated the test cases TDD FTW

--- test/hanto/student/mwcjlm/epsilon/EpsilonHantoGameTest.java
+++ test/hanto/student/mwcjlm/epsilon/EpsilonHantoGameTest.java
@@ -1,5 +1,11 @@
 package hanto.student.mwcjlm.epsilon;
 
+
+import static hanto.common.HantoPieceType.BUTTERFLY;
+import static hanto.common.HantoPieceType.CRAB;
+import static hanto.common.HantoPieceType.SPARROW;
+import static hanto.common.HantoPlayerColor.BLUE;
+import static hanto.common.HantoPlayerColor.RED;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import hanto.common.HantoException;
@@ -7,6 +13,7 @@ import hanto.common.HantoGameID;
 import hanto.common.HantoPieceType;
 import hanto.common.HantoPlayerColor;
 import hanto.common.HantoPrematureResignationException;
+import hanto.common.MoveResult;
 import hanto.studentmwcjlm.common.ComparableHantoCoordinate;
 
 import java.util.ArrayList;
@@ -16,6 +23,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import common.HantoTestGame;
+import common.HantoTestGame.PieceLocationPair;
 import common.HantoTestGameFactory;
 
 public class EpsilonHantoGameTest {
@@ -87,7 +95,107 @@ public class EpsilonHantoGameTest {
 		game.makeMove(null, null, null);
 	}
 	
+	@Test
+	public void shouldBeAbleToPlaceHorse() throws HantoException {
+		initWithButterflies();
+		//0, -1
+		assertEquals(MoveResult.OK, game.makeMove(HantoPieceType.HORSE, null, createCoord(0, -1))); 
+	}
+	
+	@Test
+	public void shouldBeAbleToPlaceSparrow() throws HantoException {
+		initWithButterflies();
+		//0, -1
+		assertEquals(MoveResult.OK, game.makeMove(HantoPieceType.SPARROW, null, createCoord(0, -1))); 
+	}
+	
+	@Test
+	public void shouldBeAbleToPlaceCrab() throws HantoException {
+		initWithButterflies();
+		//0, -1
+		assertEquals(MoveResult.OK, game.makeMove(HantoPieceType.CRAB, null, createCoord(0, -1))); 
+	}
+	
+	@Test
+	public void blueShouldBeAbleToResign() throws HantoException {
+		final PieceLocationPair[] board = new PieceLocationPair[] {
+			    plPair(BLUE, BUTTERFLY, 0, 0), 
+			    plPair(RED, CRAB, 1, 0),
+			    plPair(RED, CRAB, 0, -1),
+			    plPair(RED, CRAB, -1, 1)
+			    
+		};
+		game.initializeBoard(board);
+		game.setPlayerMoving(BLUE);		
+		game.makeMove(null, null, null);
+	}
+	
+	@Test
+	public void blueSparrowShouldBeAbleToResign() throws HantoException {
+		final PieceLocationPair[] board = new PieceLocationPair[] {
+			    plPair(BLUE, SPARROW, 0, 0), 
+			    plPair(RED, CRAB, 1, 0),
+			    plPair(RED, CRAB, 0, -1),
+			    plPair(RED, CRAB, -1, 1)
+			    
+		};
+		game.initializeBoard(board);
+		game.setPlayerMoving(BLUE);		
+		game.makeMove(null, null, null);
+	}
+	
+	@Test(expected = HantoException.class)
+	public void placingCraneShouldThrowException() throws HantoException {
+		initWithButterflies();
+		game.makeMove(HantoPieceType.CRANE, null, createCoord(0,-1));
+	}
+	
+	@Test
+	public void sparrowShouldOnlyBeableToFlyFourSpaces() throws HantoException {
+		final PieceLocationPair[] board = new PieceLocationPair[] {
+			    plPair(BLUE, BUTTERFLY, 0, 0), 
+			    plPair(RED, BUTTERFLY, 0, 1),
+			    plPair(RED, CRAB, 0, 2),
+			  //  plPair(RED, CRAB, 0, 3),
+			    plPair(BLUE, SPARROW, 0, -1)			    
+		};
+		game.initializeBoard(board);
+		game.setPlayerMoving(BLUE);		
+		assertEquals(MoveResult.OK, game.makeMove(SPARROW, createCoord(0,  -1), createCoord(0, 3)));
+		
+	}
+	
+	@Test(expected = HantoException.class)
+	public void sparrowShouldNotBeableToFlyFiveSpaces() throws HantoException {
+		final PieceLocationPair[] board = new PieceLocationPair[] {
+			    plPair(BLUE, BUTTERFLY, 0, 0), 
+			    plPair(RED, BUTTERFLY, 0, 1),
+			    plPair(RED, CRAB, 0, 2),
+			    plPair(RED, CRAB, 0, 3),
+			    plPair(BLUE, SPARROW, 0, -1)			    
+		};
+		game.initializeBoard(board);
+		game.setPlayerMoving(BLUE);		
+		game.makeMove(SPARROW, createCoord(0,  -1), createCoord(0, 4));
+		
+	}
+	
 	public ComparableHantoCoordinate createCoord(int x, int y) {
 		return new ComparableHantoCoordinate(x,y);
 	}
+	
+	/**
+	 * Factory method to create a piece location pair.
+	 * @param player the player color
+	 * @param pieceType the piece type
+	 * @param x starting location
+	 * @param y end location
+	 * @return
+	 */
+	private PieceLocationPair plPair(HantoPlayerColor player, HantoPieceType pieceType, 
+			int x, int y)
+	{
+		return new PieceLocationPair(player, pieceType, new ComparableHantoCoordinate(x, y));
+	}
+	
 }
